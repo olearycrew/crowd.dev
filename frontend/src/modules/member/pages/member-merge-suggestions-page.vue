753bc22e9695131f61df35058a96d0f64eb7110e
Gasper Grom <gasper.grom@gmail.com>
2023-10-23 16:29:43 +0700

    Primary is set before merging

--- frontend/src/modules/member/pages/member-merge-suggestions-page.vue
+++ frontend/src/modules/member/pages/member-merge-suggestions-page.vue
@@ -248,12 +248,12 @@ const mergeSuggestion = () => {
     return;
   }
   sendingMerge.value = true;
-  primary.value = 0;
   MemberService.merge(
     membersToMerge.value.members[primary.value],
     membersToMerge.value.members[(primary.value + 1) % 2],
   )
     .then(() => {
+      primary.value = 0;
       Message.success('Members merged successfuly');
       fetch();
     })
