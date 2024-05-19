5620eb11db1b78f1b7c1c76f50e80cf0b4db4da4
Misha Savelyev <mikhail@crowd.dev>
2024-05-19 07:56:38 +0000

    Fetch orgs from postgres for merge suggestion generation

--- services/libs/data-access-layer/src/orgs/index.ts
+++ services/libs/data-access-layer/src/orgs/index.ts
@@ -1,7 +1,8 @@
 import { getEnv } from '@crowd/common'
 import { DbStore } from '@crowd/database'
 import { IOrganization } from '@crowd/types'
-import { IQueryNumberOfNewOrganizations } from './types'
+import { IOrganizationPartialAggregatesRawResult, IQueryNumberOfNewOrganizations } from './types'
+import { QueryExecutor } from '../queryExecutor'
 
 const s3Url = `https://${
   process.env['CROWD_S3_MICROSERVICES_ASSETS_BUCKET']
@@ -9,7 +10,7 @@ const s3Url = `https://${
 
 export async function getOrganizationById(db: DbStore, id: string): Promise<IOrganization> {
   const query = `
-    SELECT 
+    SELECT
       "id", "tenantId", "displayName",
       "logo" AS "avatarUrl",
       "createdAt"
@@ -52,4 +53,51 @@ export async function getNumberOfNewOrganizations(
   })
 
   return rows[0].count
+}
+
+export async function findOrgsForMergeSuggestions(
+  qx: QueryExecutor,
+  tenantId: string,
+  batchSize: number,
+  afterOrganizationId?: string,
+  lastGeneratedAt?: string,
+): Promise<IOrganizationPartialAggregatesRawResult[]> {
+  let filter = ''
+  if (afterOrganizationId) {
+    filter += `AND o.id > $(afterOrganizationId)`
+  }
+
+  if (lastGeneratedAt) {
+    filter += 'AND o."createdAt" > $(lastGeneratedAt)'
+  }
+
+  return qx.select(
+    `
+      SELECT
+        o.id,
+        json_agg(oi) as identities,
+        ARRAY_AGG(DISTINCT onm."noMergeId") AS "noMergeIds",
+        o."displayName",
+        o.location,
+        o.industry,
+        o.website,
+        o.ticker,
+        osa."activityCount"
+      FROM organizations o
+      JOIN "organizationSegmentsAgg" osa ON o.id = osa."organizationId"
+      JOIN "organizationNoMerge" onm ON onm."organizationId" = o.id
+      JOIN "organizationIdentities" oi ON o.id = oi."organizationId"
+      WHERE o."tenantId" = $(tenantId)
+        ${filter}
+      GROUP BY o.id, osa.id
+      ORDER BY o.id
+      LIMIT $(batchSize)
+    `,
+    {
+      tenantId,
+      batchSize,
+      afterOrganizationId,
+      lastGeneratedAt,
+    },
+  )
 }
--- services/libs/data-access-layer/src/orgs/types.ts
+++ services/libs/data-access-layer/src/orgs/types.ts
@@ -1,5 +1,20 @@
+import { IOrganizationIdentity } from '@crowd/types'
+
 export interface IQueryNumberOfNewOrganizations {
   tenantId: string
   after?: Date
   before?: Date
+}
+
+export interface IOrganizationPartialAggregatesRawResult {
+  id: string
+  identities: IOrganizationIdentity[]
+  noMergeIds: string[]
+  displayName: string
+
+  location: string
+  industry: string
+  website: string
+  ticker: string
+  activityCount: number
 }
